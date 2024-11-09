a9c88731f07a4d5454524cc008586008a4bf5e6a
BetulAktoprak <145235075+BetulAktoprak@users.noreply.github.com>
2024-11-10 02:16:42 +0300

    BenchmarkDotNet paketi uygulaması

---
+++ BenchMark.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace BenchMark.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        BenchmarkRunner.Run<BenchmarkService>();
+    }
+}
