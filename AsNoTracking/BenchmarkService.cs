20ad74f5e1c676d8e0221f63e26757f21f6aabc6
BetulAktoprak <145235075+BetulAktoprak@users.noreply.github.com>
2024-11-10 02:51:51 +0300

    AsNoTracking mekanizması

---
+++ AsNoTracking/BenchmarkService.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsNoTracking;
+
+[ShortRunJob, Config(typeof(Config))]
+public class BenchmarkService
+{
+    private class Config : ManualConfig
+    {
+        public Config()
+        {
+            SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithRatioStyle(BenchmarkDotNet.Columns.RatioStyle.Trend);
+        }
+    }
+
+    [Benchmark(Baseline = true)]
+    public void GetAllWithTracking()
+    {
+        AppDbContext context = new();
+        context.Products.ToList();
+    }
+
+    [Benchmark]
+    public void GetAllWithAsNoTracking()
+    {
+        AppDbContext context = new();
+        context.Products.AsNoTracking().ToList();
+    }
+
+    //[Benchmark(Baseline = true)]
+    //public void GetFirstWithTracking()
+    //{
+    //    AppDbContext context = new();
+    //    context.Products.First();
+    //}
+
+    //[Benchmark]
+    //public void GetFirstWithAsNoTracking()
+    //{
+    //    AppDbContext context = new();
+    //    context.Products.AsNoTracking().First();
+    //}
+}
