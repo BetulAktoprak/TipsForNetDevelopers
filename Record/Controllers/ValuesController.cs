78183f5d2b798bbf9431ebde946575b584c686fa
BetulAktoprak <145235075+BetulAktoprak@users.noreply.github.com>
2024-11-25 22:23:13 +0300

    Record

---
+++ Record/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Record.DTOs;
+
+namespace Record.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpPost("[action]")]
+    public IActionResult LoginWithRecord(Login login)
+    {
+        return Ok("Giriş başarılı");
+    }
+}
---
+++ Record/DTOs/LoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Record.DTOs;
+
+public record class Login(
+    string Email,
+    string Password,
+    bool RememberMe = true);
+
