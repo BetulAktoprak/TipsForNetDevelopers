b1764e21a1a810cd1c986198a56ca36c254d7a68
BetulAktoprak <145235075+BetulAktoprak@users.noreply.github.com>
2024-11-25 21:44:15 +0300

    ErrorLog

---
+++ DataAccess/Models/ErrorLog.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccess.Models;
+public sealed class ErrorLog
+{
+    public int Id { get; set; }
+    public string Trace { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public string MethodName { get; set; } = string.Empty;
+}
