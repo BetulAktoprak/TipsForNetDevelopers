78c0b8015fb8e17cc381710cbdcde61c0beb35f6
BetulAktoprak <145235075+BetulAktoprak@users.noreply.github.com>
2024-11-25 22:03:34 +0300

    IQueryable

---
+++ IQueryable/Program.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+
+namespace IQueryable;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        AppDbContext context = new();
+
+        IQueryable<Product> products = context.Products.AsQueryable();
+
+        IList<Product> productList = products.Where(p => p.Id > 500 && p.Id < 600).ToList();
+
+        Console.WriteLine("Hello, World!");
+    }
+}
